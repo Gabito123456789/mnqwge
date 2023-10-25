cd8e40939603852d3b8be91c2e675fa135ef8538
Gabito123456789 <94557006+Gabito123456789@users.noreply.github.com>
2023-10-25 18:18:58 -0300

    .

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button } from 'react-native';
 
-export default function App() {
+export default function TextInverterApp() {
+  const [inputText, setInputText] = useState('');
+  const [outputText, setOutputText] = useState('');
+
+  const invertText = () => {
+    const invertedText = inputText.split('').reverse().join('');
+    setOutputText(invertedText);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Texto a Invertir:</Text>
+      <TextInput
+        value={inputText}
+        onChangeText={text => setInputText(text)}
+        style={{ borderWidth: 1, width: 200, padding: 5, margin: 10 }}
+      />
+      <Button
+        title="Invertir Texto"
+        onPress={invertText}
+      />
+      <Text style={{ marginTop: 20 }}>Texto Invertido:</Text>
+      <Text>{outputText}</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
